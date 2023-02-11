068574183433604488dca74854784c6674b4d79b
Itamar1234 <Itamardarihess@gmail.com>
2023-02-11 21:04:29 +0200

    cors changed

--- src/components/manage-backend.jsx
+++ src/components/manage-backend.jsx
@@ -1,7 +1,7 @@
 import store from "../redux/store";
 import {useDispatch} from "react-redux";
 import io from "socket.io-client";
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://space-game123.netlify.app/");
 
 const ManageBackend = () => {
     const dispatch = useDispatch();
