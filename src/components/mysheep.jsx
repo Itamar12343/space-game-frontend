924f1d07eef85aebdd4dedeb6bcc089aaa776ef3
Itamar1234 <Itamardarihess@gmail.com>
2023-02-25 00:03:05 +0200

    delay fixed

--- src/components/mysheep.jsx
+++ src/components/mysheep.jsx
@@ -4,6 +4,7 @@ import "../style/mysheep.css";
 import {RocketTakeoffFill} from "react-bootstrap-icons";
 import { useState } from "react";
 import { useRef } from "react";
+import { useEffect } from "react";
 
 const Mysheep = () => {
     const [left, setleft] = useState(50);
@@ -12,13 +13,16 @@ const Mysheep = () => {
     const [prevRoom, setPrevRoom] = useState(null);
     const shootref = useRef(null);
     const [shoot_wait, setShootWait] = useState(false);
+    const [shoot_left, setShoot_left] = useState(50);
 
     const unsubscribe = store.subscribe(()=>{
         let gotRoomState = store.getState().AproveRoomReducer;
         
         if(gotRoomState === true && gotRoomState !== prevRoom){
-            setRoomState(true);
-            setPrevRoom(true);
+            setTimeout(() => {
+                setRoomState(true);
+                setPrevRoom(true);
+            }, 3000);
         }
         if(gotRoomState === false && gotRoomState !== prevRoom){
             setRoomState(false);
@@ -27,17 +31,24 @@ const Mysheep = () => {
         unsubscribe();
     });
 
+    useEffect(()=>{
+        console.log(shoot_wait);
+    },[shoot_wait]);
+
     document.onkeydown = (e)=>{
         if(RoomState === true){
         let key = e.key;
 
         if(key === "Enter"){
             if(shoot_wait === false){
-            setShootWait(true);
-            dispatch({type: "setShootTrue"});
-            dispatch({type: "setShootFalse"});
-            shootref.current.classList.add("my-shoot-animation");
-            setTimeout(() => {
+                setShoot_left(left);
+                setShootWait(true);
+                dispatch({type: "setShootTrue"});
+                setTimeout(() => {
+                dispatch({type: "setShootFalse"});
+                }, 100);
+                shootref.current.classList.add("my-shoot-animation");
+                setTimeout(() => {
                 shootref.current.classList.remove("my-shoot-animation");
                 setShootWait(false);
             }, 400);
@@ -47,6 +58,9 @@ const Mysheep = () => {
         if(key === "ArrowRight"){
             if(left < 90){
                 let leftPosition = left + 10;
+                if(shoot_wait === false){
+                    setShoot_left(leftPosition);
+                }
                 dispatch({type: "set-position", text: leftPosition});
                 setleft(prev=> prev + 10);
             }
@@ -54,6 +68,9 @@ const Mysheep = () => {
         if(key === "ArrowLeft"){
             if(left > 10){
                 let leftPosition = left - 10;
+                if(shoot_wait === false){
+                    setShoot_left(leftPosition);
+                }
                 dispatch({type: "set-position", text: leftPosition});
                 setleft(prev=> prev - 10);
             }
@@ -64,7 +81,7 @@ const Mysheep = () => {
     return ( 
         <div>
             <RocketTakeoffFill style={{left: left ? `${left}%` : "50%"}} className="mysheep"/>
-            <div ref={shootref} style={{left: left ? `${left}%` : "50%"}} className="my-shoot"></div>
+            <div ref={shootref} style={{left: shoot_left ? `${shoot_left}%` : "50%"}} className="my-shoot"></div>
         </div>
      );
 }
